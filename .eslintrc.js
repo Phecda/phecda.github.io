ce4db18849c8865c1ffa7fea50850690c6a84a75
Phecda <szp12345141@gmail.com>
2022-04-21 01:23:41 +0800

    chore: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/**
+ * @type import('eslint').Linter.Config
+ */
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  env: {
+    es6: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'preact',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    host: '0.0.0.0',
+  },
 });
