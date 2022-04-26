602a106b16da347f6a18d7e44bb428fb11a50429
Phecda <szp12345141@gmail.com>
2022-04-26 19:32:17 +0800

    feat: add ua parser

--- src/pages/DeviceInspector/index.tsx
+++ src/pages/DeviceInspector/index.tsx
@@ -1,11 +1,39 @@
+import { useMemo } from 'preact/hooks';
+import parser from 'ua-parser-js';
 import styles from './styles.module.css';
 
 export default function DeviceInspector() {
+  const { ua, browser, cpu, device, engine, os } = useMemo(() => parser(), []);
   return (
     <div className={`safe-background bg-below-top-bar ${styles.background}`}>
       <div className="st-content wide-content">
         <h1>Your device</h1>
-        <p>{navigator.userAgent}</p>
+        <dl className={styles.descList}>
+          <dt>User Agent</dt>
+          <dd>{ua}</dd>
+          <dt>OS</dt>
+          <dd>
+            {os.name}, {os.version}
+          </dd>
+          <dt>Browser</dt>
+          <dd>
+            {browser.name}, {browser.version}
+          </dd>
+          <dt>Engine</dt>
+          <dd>
+            {engine.name}, {engine.version}
+          </dd>
+          <dt>CPU</dt>
+          <dd>{cpu.architecture || '-'}</dd>
+          <dt>Device</dt>
+          <dd>
+            {device.type
+              ? `(${device.type}) ${device.vendor ?? '-'}, ${
+                  device.model ?? '-'
+                }`
+              : '-'}
+          </dd>
+        </dl>
       </div>
     </div>
   );
