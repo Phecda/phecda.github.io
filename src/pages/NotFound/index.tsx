926b755100cb8088eee82de1ccb3b8b8b78c6ed0
Phecda <szp12345141@gmail.com>
2022-04-30 17:25:42 +0800

    refactor: NotFound style

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,11 +2,13 @@ import { Link } from 'preact-router';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Emm... Why are you here?</h1>
-      <p>
-        Let`s go back to <Link href="/">Home</Link>
-      </p>
+    <div className="safe-background bg-below-top-bar">
+      <main className="st-content wide-content">
+        <h1>Emm... Why are you here?</h1>
+        <p>
+          Let`s go back to <Link href="/">Home</Link>
+        </p>
+      </main> 
     </div>
   );
 }
