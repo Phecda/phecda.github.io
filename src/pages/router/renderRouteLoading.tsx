d0886b2fdf578b9087cf8bde0f59b79012569e9f
Phecda <szp12345141@gmail.com>
2022-04-30 14:30:51 +0800

    feat: pwa configs

--- src/pages/router/renderRouteLoading.tsx
+++ src/pages/router/renderRouteLoading.tsx
@@ -1,8 +1,9 @@
 export function renderRouteLoading() {
   return (
-    <div className="safe-padding">
-      <div className="st-content">
+    <div className="safe-padding bg-below-top-bar">
+      <div className="st-content wide-content">
         <h1>Just a moment...</h1>
+        <p>Wait for too long? Check your browser!</p>
       </div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,45 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 import preact from '@preact/preset-vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'Phecda`s playground',
+        short_name: 'Playground',
+        description: 'A web app to try all possibilities.',
+        theme_color: '#673ab8',
+        background_color: '#673ab8',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: '0.0.0.0',
     port: 3090,
