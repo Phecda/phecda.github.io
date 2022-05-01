cd6e43643b2d0037f33fd4c6661c9d4a9b86a9a8
Phecda <szp12345141@gmail.com>
2022-05-01 15:25:51 +0800

    feat: splash screen for ios

---
+++ scripts/generatePWASplash.mjs
@@ -0,0 +1,22 @@
+import pwaGenerator from 'pwa-asset-generator';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+console.log('directory-name 👉️', __dirname);
+
+const source = path.join(__dirname, '../public/favicon.svg');
+const outputPath = path.join(__dirname, '../public/');
+const indexHTMLPath = path.join(__dirname, '../index.html');
+
+pwaGenerator.generateImages(source, outputPath, {
+  background: '#673ab8',
+  index: indexHTMLPath,
+  type: 'png',
+  splashOnly: true,
+  pathOverride: '',
+  padding: 'calc(50vmin - 20%)',
+});
