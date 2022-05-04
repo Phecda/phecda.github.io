409820c529317d242d0c3a8525d69104e8a38a01
Phecda <szp12345141@gmail.com>
2022-05-04 14:58:44 +0800

    feat: khaenriah text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,9 @@
-import { Link } from 'preact-router';
-
 export default function Home() {
   return (
     <div className="safe-background bg-below-top-bar">
       <main className="st-content wide-content">
-        <h1>Welcome, Traveler!</h1>
-        <p>
-          But it is another page <Link href="/device">here</Link>
-        </p>
+        <h1 className="khaenriah-text">Welcome, Traveler!</h1>
+        <p className="khaenriah-text">Ad astra abyssosque</p>
       </main>
     </div>
   );
